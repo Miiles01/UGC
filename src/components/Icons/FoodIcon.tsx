835962d1e279b2c065e9781e68569a601e0eb83e
Miiles01 <Miiles01@users.noreply.github.com>
2026-09-07 17:19:23 -0600

    feat: Add custom FoodIcon (plato) SVG and apply it to mobile carousel reels

---
+++ src/components/Icons/FoodIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const FoodIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg 
+    viewBox="0 -4.83 52 52" 
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g transform="translate(-788.946 -1785.428)">
+      <path d="M814.946,1793.095a24,24,0,0,0-24,24h48A24,24,0,0,0,814.946,1793.095Z" fill="transparent" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4"/>
+      <line x2="48" transform="translate(790.946 1825.761)" fill="transparent" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4"/>
+      <line y2="5.667" transform="translate(814.946 1787.428)" fill="transparent" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4"/>
+    </g>
+  </svg>
+);
