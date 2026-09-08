e08a449682c43a752176e99ea1d5017e11fdce4f
Miiles01 <Miiles01@users.noreply.github.com>
2026-09-08 11:53:22 -0600

    style: Update Facebook icon to use specific SVG repo design requested by user

--- src/components/Plantillas/Plantilla01.tsx
+++ src/components/Plantillas/Plantilla01.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Menu, Instagram, Twitter, Mail, Play, X, Facebook } from 'lucide-react';
+import { Menu, Instagram, Twitter, Mail, Play, X } from 'lucide-react';
+import { FacebookIcon } from '../Icons/FacebookIcon';
 import { Tiktok } from '../Icons/Tiktok';
 import { PortfolioSmoothScroll } from '../Portfolio/PortfolioSmoothScroll';
 import { AnimatePresence, motion } from 'framer-motion';
@@ -887,7 +888,7 @@ const testimonials = [
                       <Tiktok className="w-5 h-5" />
                     </a>
                     <a href="#" className="p-3 bg-black/5 dark:bg-white/5 rounded-full hover:bg-black/10 dark:hover:bg-white/10 transition-colors">
-                      <Facebook className="w-5 h-5" />
+                      <FacebookIcon className="w-5 h-5" />
                     </a>
                   </div>
                 </div>
@@ -925,7 +926,7 @@ const testimonials = [
                     <Tiktok className="w-4 h-4" /> TikTok
                   </a>
                   <a href="#" className="hover:opacity-100 transition-opacity flex items-center gap-2">
-                    <Facebook className="w-4 h-4" /> Facebook
+                    <FacebookIcon className="w-4 h-4" /> Facebook
                   </a>
                 </div>
               </div>
